d1d54842427c12d6deb069b026cc18cca033f1c8
Ar1f007 <Muhammadarif9416@yahoo.com>
2021-01-23 18:59:07 +0600

    middleware authentication done

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -11,19 +11,22 @@ if(Session::has('user')){
 
 <nav class="navbar navbar-default">
     <div class="container-fluid">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">E-com</a>
-      </div>
+      
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="">E-com</a>
+        </div>
+      
   
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        @if (Session::has('user'))
         <ul class="nav navbar-nav">
           <li><a href="/">Home <span class="sr-only">(current)</span></a></li>
           <li><a href="/orderList">Order</a></li>
@@ -35,20 +38,23 @@ if(Session::has('user')){
           <button type="submit" class="btn btn-default">Search</button>
         </form>
         <ul class="nav navbar-nav navbar-right">
+          
           <li><a href="/cartList">cart({{$total}})</a></li>
-          @if (Session::has('user'))
           <li class="dropdown">
             <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{Session::get('user')['name']}}
             <span class="caret"></span></a>
             <ul class="dropdown-menu">
               <li><a href="/logout">Logout</a></li>
             </ul>
-          </li>             
+          </li>
+        </ul>             
           @else
+          <ul class="nav navbar-nav navbar-right">
           <li><a href="/login">Login</a></li>
           <li><a href="/register">Register</a></li>
-          @endif   
-        </ul>
+          @endif
+        </ul>    
+        
       </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
   </nav>
